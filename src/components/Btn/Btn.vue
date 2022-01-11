672f6f0a1aae122fe3c19de3eea04f09ba120d1b
Justin <justinanthony.primary@gmail.com>
2022-01-11 22:52:14 +0800

    [feat]: button component

---
+++ src/components/Btn/Btn.vue
@@ -0,0 +1,18 @@
+<template>
+    <button :class='this.btnClass'>
+      {{ btnVal }}
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'Btn',
+  props: ['value', 'class'],
+  data() {
+    return {
+      btnVal: this.value,
+      btnClass: this.class,
+    };
+  }
+};
+</script>
